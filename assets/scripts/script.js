e7faa27338237f49f29bc79fd4519dc944ce3c7a
Vinicius <vinycius1993@gmail.com>
2023-01-17 15:35:48 -0300

    implementado a função que validará os campos obrigatórios

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -11,11 +11,11 @@ class Usuario {
         
     //Função com a lista de eventos a serem executadas.
     addEventos () {
-        document.getElementById("btnCadastrar").addEventListener("click", usuario.clicar)
+        document.getElementById("btnCadastrar").addEventListener("click", usuario.salvar)
+        
     }
-/*    let btn = document.querySelector('#btn-cadastrar')
-    btn.addEventListener('submit', usuario.salvar)*/
-    //Função para capturar todos os inputs do formulário.    
+    
+    //Função para capturar todos os inputs do formulário.
     lerDados() {
         let usuario = {}
             
@@ -27,8 +27,8 @@ class Usuario {
             usuario.cidade = document.getElementById('cidade').value
             usuario.bairro = document.getElementById('bairro').value
             usuario.logradouro = document.getElementById('logradouro').value
-            usuario.numero = document.getElementById('numero').value            
-
+            usuario.numero = document.getElementById('numero').value
+            
             return usuario
     }
     
@@ -36,10 +36,19 @@ class Usuario {
         let msg = ''
         
         if (usuario.nome == '') {
-            `${msg} Informe o seu Nome`
+            msg += 'Informe o seu Nome \n'
         }
         if (usuario.email == '') {
             msg += 'Informe o seu E-mail \n'
+        }
+        if (usuario.telefone == '') {
+            msg += 'Informe o seu Telefone \n'
+        }
+        if (usuario.cpf == '') {
+            msg += 'Informe o seu CPF \n'
+        }
+        if (usuario.cep == '') {
+            msg += 'Informe o seu CEP \n'
         }
         if (msg != '') {
             alert(msg)
@@ -47,12 +56,8 @@ class Usuario {
         }
         return true
     }
-
-    clicar() {
-        alert("Clicou!")
-    }
+    
     salvar() {
-
         let usuario = this.lerDados()
         if (this.validarCampos(usuario) == true) {
                 alert ('OOKSS')
@@ -60,44 +65,6 @@ class Usuario {
                 alert('Campos Vazios')
             }
     }
-
 }
 var usuario = new Usuario()
 window.addEventListener("load", usuario.addEventos)
-/*
-    validarCampos(usuario) {
-    //Variáveis para receber todos os valores do formulário
-    var nome = formulario-cadastro.nome.value;    
-    var email = formulario-cadastro.email.value;
-    var telefone = formulario-cadastro.telefone.value;
-    var cpf = formulario-cadastro.cpf.value;
-    var cep = formulario-cadastro.cep.value;
-    
-    if (nome === "") {
-        alert("O campo {Nome} é obrigatório!");
-        form_cadastro.nome.focus();
-        return false;
-    }
-    if (email === "") {
-        alert("O campo {E-mail} é obrigatório!");
-        form_cadastro.email.focus();
-        return false;
-    }
-    if (telefone === "") {
-        alert("O campo {Telefone} é obrigatório!");
-        form_cadastro.telefone.focus();
-        return false;
-    }
-    if (cpf === "") {
-        alert("O campo {CPF} é obrigatório!");
-        form_cadastro.cpf.focus();
-        return false;
-    }
-    if (cep === "") {
-        alert("O campo {CEP} é obrigatório!");
-        form_cadastro.cep.focus();
-        return false;
-    }
-
-}
-*/
