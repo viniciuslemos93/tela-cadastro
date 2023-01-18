fdbc4b3ab4e2e39c8a6d58004836578e3c7ac3a9
Vinicius <vinycius1993@gmail.com>
2023-01-18 14:26:49 -0300

    Inserido o parágrafo de cokies na tela de cadastro

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -9,53 +9,95 @@ class Usuario {
     logradouro;
     numero;
         
-    /*Função com a lista de eventos a serem executadas.
+    //Função com a lista de eventos a serem executadas.
     addEventos () {
         document.getElementById("btnCadastrar").addEventListener("click", usuario.salvar)
+        document.getElementById('cep').addEventListener('focusout',usuario.consultarCep);
         
-    }*/
+    }
     salvar() {
-        let usuario = this.lerDados()
-        if (this.validarCampos(usuario) == true) {
-                //Acessar a página de login
+        let cadastro = this.lerDados()
+        if (this.validarCampos(cadastro) == true) {
+
             }else {
-                //Mostrar os campos que precisa preencher
-                
+                //Mostrar os campos que precisa preencher                
             }
     }
     //Função para capturar todos os inputs do formulário.
     lerDados() {
-        let usuario = {}
+        let dadosUsuario = {}            
+        dadosUsuario.nome = document.getElementById('nome').value
+        dadosUsuario.email = document.getElementById('email').value
+        dadosUsuario.telefone = document.getElementById('telefone').value
+        dadosUsuario.cpf = document.getElementById('cpf').value
+        dadosUsuario.cep = document.getElementById('cep').value
+        dadosUsuario.cidade = document.getElementById('cidade').value
+        dadosUsuario.bairro = document.getElementById('bairro').value
+        dadosUsuario.logradouro = document.getElementById('logradouro').value
+        dadosUsuario.numero = document.getElementById('numero').value
+        this.consultarCep(dadosUsuario.cidade, dadosUsuario.bairro, dadosUsuario.logradouro)
+            return dadosUsuario
             
-            usuario.nome = document.getElementById('nome').value
-            usuario.email = document.getElementById('email').value
-            usuario.telefone = document.getElementById('telefone').value
-            usuario.cpf = document.getElementById('cpf').value
-            usuario.cep = document.getElementById('cep').value
-            usuario.cidade = document.getElementById('cidade').value
-            usuario.bairro = document.getElementById('bairro').value
-            usuario.logradouro = document.getElementById('logradouro').value
-            usuario.numero = document.getElementById('numero').value
-            
-            return usuario
+    }
+    consultarCep(cidade, bairro, logradouro) {
+        let c = cidade;
+        let b = bairro;
+        let l = logradouro        
+        console.log(c)
+        console.log(b)
+        console.log(l)
+
+        /*
+        const hidden_show = document.querySelector('#hidden-show')
+        
+        //Função para formatar o cep que venho com a máscara do input, deixando-o apenas números para enviar a API.
+        var cep = usuario.cep.replace(/[^0-9]/g,'');
+        console.log(cep)
+        var url = 'https://viacep.com.br/ws/' + cep + '/json'
+        var request = new XMLHttpRequest()
+        request.open('GET', url)
+        request.onerror = function(e) {
+            document.getElementById('resposta').innerHTML = 'API OFFLINE ou CEP INVÁLIDO'
+            document.getElementById('resposta').style = 'color: red'
+            document.getElementById("cep").style.border = '1px solid red'
+    }
+    request.onload = () => {
+        var response = JSON.parse(request.responseText)
+        if (response.erro === true) {
+            document.getElementById('resposta').innerHTML = 'CEP NÃO ENCONTRADO'
+        } else {
+            hidden_show.style.display = 'block'
+            usuario.logradouro = response.logradouro
+            usuario.bairro = response.bairro
+            usuario.cidade = response.localidade        
+        }
     }
     
-    validarCampos(usuario) {
+    request.send();
+        usuario.logradouro = ''; // Limpa o campo
+        usuario.bairro = ''; // Limpa o campo
+        usuario.cidade = ''; // Limpa o campo    
+        hidden_show.style.display = 'none'
+        resposta.innerHTML = ''
+        */
+    }
+
+    validarCampos(dadosObrigatorios) {
         let msg = ''
         
-        if (usuario.nome == '') {
+        if (dadosObrigatorios.nome == '') {
             msg += 'Informe o seu Nome \n'
         }
-        if (usuario.email == '') {
+        if (dadosObrigatorios.email == '') {
             msg += 'Informe o seu E-mail \n'
         }
-        if (usuario.telefone == '') {
+        if (dadosObrigatorios.telefone == '') {
             msg += 'Informe o seu Telefone \n'
         }
-        if (usuario.cpf == '') {
+        if (dadosObrigatorios.cpf == '') {
             msg += 'Informe o seu CPF \n'
         }
-        if (usuario.cep == '') {
+        if (dadosObrigatorios.cep == '') {
             msg += 'Informe o seu CEP \n'
         }
         if (msg != '') {
@@ -67,4 +109,4 @@ class Usuario {
 }
 
 var usuario = new Usuario()
-//window.addEventListener("load", usuario.addEventos)
+window.addEventListener("load", usuario.addEventos)
