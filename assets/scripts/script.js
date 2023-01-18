0b8af9c9a2e93a731fe95cca9c39887de3dd8e78
Vinicius Lemos <vinycius1993@gmail.com>
2023-01-18 18:08:40 -0300

    Ajustado a implementação de puxar o CEP automaticamente após sair do input CEP

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,111 +1,142 @@
-class Usuario {
-    nome;
-    email;
-    telefone;
-    cpf;
-    cep;    
-    cidade;
-    bairro;
-    logradouro;
-    numero;
+class EventosDoUsuario {
+    constructor(window) {
+      this.window = window;
+    }
+  
+    registrarEventos() {
+      const tabelaDeEventos = [
+        {
+          elementId: "btnCadastrar",
+          event: "click",
+          eventHandler: EventosDoUsuario.salvarDados,
+        },
+        {
+          elementId: "cep",
+          event: "focusout",
+          eventHandler: EventosDoUsuario.consultarCep,
+        },
+      ];
+  
+      const document = this.window.document;
+  
+      for (const { elementId, event, eventHandler } of tabelaDeEventos) {
+        document.getElementById(elementId)?.addEventListener(event, eventHandler);
+      }
+    }
+  
+    static salvarDados() {
+      const dadosDoUsuario = EventosDoUsuario.obterDados();
+      const informacoesCorretas = EventosDoUsuario.validarDados(dadosDoUsuario);
+  
+      if (informacoesCorretas) {
+        window.location.href = "../tela-cadastro/assets/paginas/login.html";
+      } else {
         
-    //Função com a lista de eventos a serem executadas.
-    addEventos () {
-        document.getElementById("btnCadastrar").addEventListener("click", usuario.salvar)
-        document.getElementById('cep').addEventListener('focusout',usuario.consultarCep);        
+      }
     }
-    salvar() {
-        let cadastro = this.lerDados()
-        if (this.validarCampos(cadastro) == true) {
-                window.location.href = "../tela-cadastro/assets/paginas/login.html";
-            }else {
-                //Mostrar os campos que precisa preencher                
-            }
-    }    
-    //Função para capturar todos os inputs do formulário.
-    lerDados() {
-        let dadosUsuario = {}            
-        dadosUsuario.nome = document.getElementById('nome').value
-        dadosUsuario.email = document.getElementById('email').value
-        dadosUsuario.telefone = document.getElementById('telefone').value
-        dadosUsuario.cpf = document.getElementById('cpf').value
-        dadosUsuario.cep = document.getElementById('cep').value
-        dadosUsuario.cidade = document.getElementById('cidade').value
-        dadosUsuario.bairro = document.getElementById('bairro').value
-        dadosUsuario.logradouro = document.getElementById('logradouro').value
-        dadosUsuario.numero = document.getElementById('numero').value
-        this.consultarCep(dadosUsuario.cidade, dadosUsuario.bairro, dadosUsuario.logradouro)
-            return dadosUsuario
-            
+  
+    static obterDados() {
+      const dados = {};
+  
+      const propriedades = [
+        "nome",
+        "email",
+        "telefone",
+        "cpf",
+        "cep",
+        "cidade",
+        "bairro",
+        "logradouro",
+        "numero",
+      ];
+  
+      for (const propriedade of propriedades) {
+        dados[propriedade] = document.getElementById(propriedade)?.value;
+      }
+  
+      return dados;
     }
-    consultarCep(cidade, bairro, logradouro) {
-        let c = cidade;
-        let b = bairro;
-        let l = logradouro        
-        console.log(c)
-        console.log(b)
-        console.log(l)
-
-        /*
-        const hidden_show = document.querySelector('#hidden-show')
-        
-        //Função para formatar o cep que venho com a máscara do input, deixando-o apenas números para enviar a API.
-        var cep = usuario.cep.replace(/[^0-9]/g,'');
-        console.log(cep)
-        var url = 'https://viacep.com.br/ws/' + cep + '/json'
-        var request = new XMLHttpRequest()
-        request.open('GET', url)
-        request.onerror = function(e) {
-            document.getElementById('resposta').innerHTML = 'API OFFLINE ou CEP INVÁLIDO'
-            document.getElementById('resposta').style = 'color: red'
-            document.getElementById("cep").style.border = '1px solid red'
+  
+    static validarDados(dados) {
+      let msg = "";
+  
+      if (dados.nome == "") {
+        msg += "Informe o seu Nome \n";
+      }
+      if (dados.email == "") {
+        msg += "Informe o seu E-mail \n";
+      }
+      if (dados.telefone == "") {
+        msg += "Informe o seu Telefone \n";
+      }
+      if (dados.cpf == "") {
+        msg += "Informe o seu CPF \n";
+      }
+      if (dados.cep == "") {
+        msg += "Informe o seu CEP \n";
+      }
+      if (msg != "") {
+        alert(msg);
+        return false;
+      }
+      return true;
     }
-    request.onload = () => {
-        var response = JSON.parse(request.responseText)
-        if (response.erro === true) {
-            document.getElementById('resposta').innerHTML = 'CEP NÃO ENCONTRADO'
-        } else {
-            hidden_show.style.display = 'block'
-            usuario.logradouro = response.logradouro
-            usuario.bairro = response.bairro
-            usuario.cidade = response.localidade        
-        }
+  
+    static async consultarCep() {
+      const validarCep = (cep) => {
+        const cepPattern = /\d{5}\d{3}/;
+        return cepPattern.test(cep);
+      };
+  
+      const exibirErro = (mensagem) => {
+        const resposta = document.getElementById("resposta");
+        const cep = document.getElementById("cep");
+        resposta.style.display = "block";
+        resposta.innerText = mensagem;
+        resposta.style.color = "red";
+        cep.style.border = "1px solid red";
+      };
+  
+      const exibirResultado = (cepInfo) => {
+        const resposta = document.getElementById("resposta");
+        const cep = document.getElementById("cep");
+        resposta.style.display = "none";
+        cep.style.border = "1px solid #ccc";
+  
+        const logradouro = document.getElementById("logradouro");
+        const bairro = document.getElementById("bairro");
+        const cidade = document.getElementById("cidade");
+  
+        logradouro.value = cepInfo.logradouro;
+        bairro.value = cepInfo.bairro;
+        cidade.value = cepInfo.localidade;
+      };
+  
+      let { cep } = EventosDoUsuario.obterDados();
+  
+      cep = cep.replace(/\D/g, "");
+  
+      const cepOk = validarCep(cep);
+      if (!cepOk) {
+        exibirErro("CEP INVÁLIDO");
+        return;
+      }
+  
+      const resource = `https://viacep.com.br/ws/${cep}/json/`;
+  
+      await fetch(resource)
+        .then((response) => response.json())
+        .then((cepInfo) => {
+          if (cepInfo.erro) {
+            exibirErro("CEP NÃO ENCONTRADO");
+          } else {
+            exibirResultado(cepInfo);
+          }
+        })
+        .catch((err) => console.log(err));
     }
-    
-    request.send();
-        usuario.logradouro = ''; // Limpa o campo
-        usuario.bairro = ''; // Limpa o campo
-        usuario.cidade = ''; // Limpa o campo    
-        hidden_show.style.display = 'none'
-        resposta.innerHTML = ''
-        */
-    }
-
-    validarCampos(dadosObrigatorios) {
-        let msg = ''
-        
-        if (dadosObrigatorios.nome == '') {
-            msg += 'Informe o seu Nome \n'
-        }
-        if (dadosObrigatorios.email == '') {
-            msg += 'Informe o seu E-mail \n'
-        }
-        if (dadosObrigatorios.telefone == '') {
-            msg += 'Informe o seu Telefone \n'
-        }
-        if (dadosObrigatorios.cpf == '') {
-            msg += 'Informe o seu CPF \n'
-        }
-        if (dadosObrigatorios.cep == '') {
-            msg += 'Informe o seu CEP \n'
-        }
-        if (msg != '') {
-            alert(msg)
-            return false
-        }
-        return true
-    }
-}
-
-var usuario = new Usuario()
-window.addEventListener("load", usuario.addEventos)
+  }
+  
+  const eventos = new EventosDoUsuario(window);
+  eventos.registrarEventos();
+  
